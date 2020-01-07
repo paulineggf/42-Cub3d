223a80d8b9713244f8284df3a4be3a6f805d342f
Pauline GANGLOFF <pganglof@e2r10p14.42.fr>
2020-01-07 10:48:01 +0100

    modif makefile

--- minilibx_opengl_20191021/mlx_opengl.m
+++ minilibx_opengl_20191021/mlx_opengl.m
@@ -31,10 +31,10 @@ void	*mlx_new_opengl_window(mlx_ptr_t *mlx_ptr, int size_x, int size_y, char *ti
   if ((newwin = malloc(sizeof(*newwin))) == NULL)
     return ((void *)0);
   newwin->img_list = NULL;
-  newwin->next = mlx_ptr.win_list;
+  newwin->next = mlx_ptr->win_list;
   newwin->nb_flush = 0;
   newwin->pixmgt = 0;
-  mlx_ptr.win_list = newwin;
+  mlx_ptr->win_list = newwin;
 
   NSRect windowRect = NSMakeRect(100, 100, size_x, size_y);
   str = [NSString stringWithCString:title encoding:NSASCIIStringEncoding];
@@ -46,12 +46,12 @@ void	*mlx_new_opengl_window(mlx_ptr_t *mlx_ptr, int size_x, int size_y, char *ti
 
 int	mlx_opengl_swap_buffers(mlx_win_list_t *win_ptr)
 {
-  [(id)(win_ptr.winid) flushGLContext];
+  [(id)(win_ptr->winid) flushGLContext];
   return (0);
 }
 
 int	mlx_opengl_window_set_context(mlx_win_list_t *win_ptr)
 {
-  [(id)(win_ptr.winid) selectGLContext];
+  [(id)(win_ptr->winid) selectGLContext];
   return (0);
 }
